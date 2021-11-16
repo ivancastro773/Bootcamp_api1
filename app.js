3b5c571e2573f0946252a07e3cf592bd5dbd12da
unknown <ivanchinocastro773@gmail.com>
2021-11-16 17:04:14 -0300

    [add/file] new post name correo etc

--- app.js
+++ app.js
@@ -7,10 +7,23 @@ const cors = require('cors');
 const methodOverride = require('method-override');
 
 const app = express();
-
 let port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(methodOverride());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json);
+
 let users = ["lisa","homero","marge","bart"];
+
+app.post("/user/create",(req,res)=>{
+    const { name, email, alias, password} = req.body;
+    users.push(req.params.name);
+    users.push(req.params.email);
+    users.push(req.params.alias);
+    users.push(req.params.password);
+    res.send("Usuario creado!");
+});
 
 app.get("/users",(req,res)=>{
     res.send(users);
